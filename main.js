6e5574c52b65c785bc7adc456c90455750e92342
Lari72 <126209286+Lari72@users.noreply.github.com>
2023-08-22 22:59:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+class ProductManager {
+    constructor()}
+     this.products = []
+
+     addProduct (title, description, price, image,  code, stock) {
+
+      this.product.push({title, description, price, image, code, stock })
+    }
+
+    getProduct() {
+        return this.products;
+    }
+
