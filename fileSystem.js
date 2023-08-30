eb0b59bf40daae70e91897fdf954c14a661c2a06
Lari72 <126209286+Lari72@users.noreply.github.com>
2023-08-29 23:48:45 -0400

    Entrega 2

--- fileSystem.js
+++ fileSystem.js
@@ -1,12 +1,66 @@
-const fs = require('fs')
 
-const persona = async () =>{
-await fs.promises.writeFile("./persona.txt", "Hola como estas?");
-await fs.promises.appendFile("./persona.txt", "\nBien y tu?")
-let respuesta = await fs.promises.readFile("./persona.txt", "utf-8")
-console.log(respuesta)
+ import {promises as fs} from "fs"
 
+ class ProductManager{
+    constructor(){
 
-await fs.promises.unlink("./persona.txt")
-};
-persona();
+        this.path ="./productos.txt"
+        this.products = []
+
+    }
+
+    static id = 0
+
+    addProduct = async (title, description, price, thumbnail, code, stock) => {
+
+        ProductManager.id++
+
+        let newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock, 
+            id: ProductManager.id
+        };
+
+        this.products.push(newProduct)
+
+        // console.log(newProduct)
+
+        await fs.writeFile(this.path, JSON.stringify(this.products));
+    };
+
+    readProducts = async () => {
+        let respuesta = await fs.readFile(this.path, "utf-8");
+        return JSON.parse(respuesta);
+    }
+
+    getProducts = async () => {
+        let respuesta2 = await this.readProducts();
+        return console.log(respuesta2);
+       
+    }
+
+ getProductsById = async (id) => {
+    let respuesta3 = await this.readProducts();
+    if(!respuesta3.find(product => product.id === id)){
+        console.log("Producto no encontrado")
+    }else{
+        console.log(respuesta3.find(product => product.id === id));
+    }
+
+   
+
+ }
+}
+ const productos = new ProductManager();
+
+//  productos.addProduct("producto prueba1", "este es un producto prueba1", 200, "thumbnail1", "abc123", 25)
+//  productos.addProduct("producto prueba2", "este es un producto prueba2", 20, "thumbnail1", "abc123", 26)
+//  productos.addProduct("producto prueba3", "este es un producto prueba3", 20, "thumbnail1", "abc123", 27)
+
+productos.getProducts()
+
+productos.getProductsById(3)
